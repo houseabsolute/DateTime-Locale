d09c5ee7d85da0103447bbf33110daf86df53e73
Dave Rolsky <autarch@urth.org>
2017-03-05 14:18:11 -0600

    Bump version after release

--- lib/DateTime/Locale.pm
+++ lib/DateTime/Locale.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '1.14';
+our $VERSION = '1.15';
 
 use DateTime::Locale::Data;
 use DateTime::Locale::FromData;
--- lib/DateTime/Locale/Util.pm
+++ lib/DateTime/Locale/Util.pm
@@ -6,7 +6,7 @@ use namespace::autoclean 0.19 -except => ['import'];
 
 use Exporter qw( import );
 
-our $VERSION = '1.14';
+our $VERSION = '1.15';
 
 our @EXPORT_OK = 'parse_locale_code';
 
